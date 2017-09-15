dd83b2a4f145be9d27aa2efad2dcd5210b246005
hvdwolf <hvdwolf@gmail.com>
2017-09-15 12:43:41 +0200

    correct for PIE required compiling as of Lollipop

--- jni/Android.mk
+++ jni/Android.mk
@@ -3,6 +3,7 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE := stress
 LOCAL_SRC_FILES := stress.c
-LOCAL_CFLAGS += -DPACKAGE=\"stress\" -DVERSION=\"1.0.4\"
+LOCAL_CFLAGS += -DPACKAGE=\"stress\" -DVERSION=\"1.0.4\" -fPIE
+LOCAL_LDFLAGS += -pie
 
 include $(BUILD_EXECUTABLE)
